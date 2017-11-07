c7b627332b70d09d7c1e85a115070bcaa5137327
Tibor Czirjak <cziti95@gmail.com>
2017-11-07 01:10:53 +0100

    half finished navigation drawer & add blank fragments

--- app/src/main/java/rft/unideb/unsolus/MainActivity.java
+++ app/src/main/java/rft/unideb/unsolus/MainActivity.java
@@ -1,8 +1,11 @@
 package rft.unideb.unsolus;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,18 +15,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import rft.unideb.unsolus.fragments.GamesFragment;
+import rft.unideb.unsolus.fragments.HomeFragment;
+import rft.unideb.unsolus.fragments.PlayersFragment;
+import rft.unideb.unsolus.fragments.TeamsFragment;
+
+public class MainActivity extends AppCompatActivity {
+  //      implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private TextView txtName, txtEmail; //later for user name and email
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+
+    public static int navItemIndex = 0;
+
+    private static final String TAG_Home = "home";
+    private static final String TAG_Games = "games";
+    private static final String TAG_Players = "players";
+    private static final String TAG_Teams = "teams";
+    private static final String TAG_Settings = "settings";
+    public static String CURRENT_TAG = TAG_Home;
+
+    private String[] activityTitles;
+
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mHandler = new Handler();
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,30 +66,191 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+      //  navigationView.setNavigationItemSelectedListener(this);
+
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
+
+        loadNavHeader();
+
+        setUpNavigationView();
+
+
+        if (savedInstanceState == null){
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_Home;
+            loadHomeFragment();
+        }
+    }
+
+    private void loadNavHeader() {
+    }
+
+    private void loadHomeFragment() {
+        selectNavMenu();
+        setToolbarTitle();
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null){
+            drawer.closeDrawers();
+
+            toggleFab();
+            return;
+        }
+
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+
+            }
+        };
+
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        toggleFab();
+
+        drawer.closeDrawers();
+
+        invalidateOptionsMenu();
+
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex){
+            case 0:
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                GamesFragment gameFragment = new GamesFragment();
+                return gameFragment;
+            case 2:
+                PlayersFragment playersFragment = new PlayersFragment();
+                return playersFragment;
+            case 3:
+                TeamsFragment teamsFragment = new TeamsFragment();
+                return teamsFragment;
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    private void toggleFab() {
+        if (navItemIndex == 0)
+            fab.show();
+        else
+            fab.hide();
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressWarnings("StatementWithEmptyBody")
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                int id = item.getItemId();
+
+                switch (id){
+                    case R.id.nav_home:
+                        navItemIndex=0;
+                        CURRENT_TAG = TAG_Home;
+                        break;
+                    case R.id.nav_games:
+                        navItemIndex=1;
+                        CURRENT_TAG=TAG_Games;
+                        break;
+                    case R.id.nav_players:
+                        navItemIndex=2;
+                        CURRENT_TAG=TAG_Players;
+                        break;
+                    case R.id.nav_teams:
+                        navItemIndex=3;
+                        CURRENT_TAG=TAG_Teams;
+                        break;
+                    default:
+                        navItemIndex=0;
+                }
+
+                if (item.isChecked()){
+                    item.setChecked(false);
+                }else{
+                    item.setChecked(true);
+                }
+                item.setChecked(true);
+
+                loadHomeFragment();
+
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            return;
         }
+        if (shouldLoadHomeFragOnBackPress){
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_Home;
+                loadHomeFragment();
+                return;
+            }
+        }
+            super.onBackPressed();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        if (navItemIndex == 0){
+            getMenuInflater().inflate(R.menu.main, menu);
+        }
+        //not finished yet
+        // TODO:
         return true;
     }
 
@@ -67,21 +262,27 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            return true;
+        }
+        if (id == R.id.action_logout){
+            Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
+    /*
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_games) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
+        } else if (id == R.id.nav_games) {
+
         } else if (id == R.id.nav_players) {
 
         } else if (id == R.id.nav_teams) {
@@ -95,5 +296,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
+    }*/
 }
