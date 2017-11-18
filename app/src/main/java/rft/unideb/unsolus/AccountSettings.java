40d5e1f60592e6ed585558bce0424f86f5788e7e
Tibor Czirjak <cziti95@gmail.com>
2017-11-18 23:37:07 +0100

    expandable listview for account settings

--- app/src/main/java/rft/unideb/unsolus/AccountSettings.java
+++ app/src/main/java/rft/unideb/unsolus/AccountSettings.java
@@ -2,12 +2,53 @@ package rft.unideb.unsolus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import rft.unideb.unsolus.others.ExpandableListAdapter;
 
 public class AccountSettings extends AppCompatActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+
+        expListView = (ExpandableListView) findViewById(R.id.setting_expendableLV);
+
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        expListView.setAdapter(listAdapter);
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Change Password");
+        listDataHeader.add("Games settings");
+
+        List<String> pwChange = new ArrayList<String>();
+        pwChange.add("Old password: ");
+        pwChange.add("New password: ");
+        pwChange.add("Confirm new password: ");
+
+        List<String> games = new ArrayList<String>();
+        games.add("Steam account: ");
+        games.add("Battle.net account: ");
+        games.add("Uplay account: ");
+
+        listDataChild.put(listDataHeader.get(0), pwChange);
+        listDataChild.put(listDataHeader.get(1), games);
     }
 }
